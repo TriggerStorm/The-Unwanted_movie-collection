d334cd765810e88e9ccd712622d718af900333fc
speedwolf10 <nicl1291@easv365.dk>
2020-01-08 01:51:43 +0100

    Gui
    gui view scene 1 with  id's  wasn't able to figure out how to switch scenes and do much yet

--- src/the/unwanted_movie/collection/FXMLDocumentController.java
+++ src/the/unwanted_movie/collection/FXMLDocumentController.java
@@ -10,7 +10,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -22,9 +25,25 @@ public class FXMLDocumentController implements Initializable {
     private Label label;
     
     @FXML
+    private Button tc_edit;
+    
+    @FXML
+    
+    private TableColumn tc_name;
+         
+    @FXML
+    private TableColumn tc_cat;
+    
+    @FXML
+    private TableColumn tc_rat;
+    @FXML
+    private TextField tf_search;
+    
+    @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+        
     }
     
     @Override
