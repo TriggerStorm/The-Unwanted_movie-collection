678ed8ce5744d9319939b40a5867971ab5189ceb
SnowBoy73 <alan4999@gmail.com>
2020-01-09 17:36:10 +0100

    Update VideoPlayerController.java
    some error with ExceptionDialog import

--- src/unwanted_mc/gui/controller/VideoPlayerController.java
+++ src/unwanted_mc/gui/controller/VideoPlayerController.java
@@ -5,10 +5,83 @@
  */
 package unwanted_mc.gui.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.util.Duration;
+//import org.controlsfx.dialog.ExceptionDialog;
+//import javafx.scene.control.Dialog<ButtonType>;
+
 /**
- *
- * @author admin
+ * @author Niclas, Martin, Michael and Alan
  */
+
+
 public class VideoPlayerController {
+    @FXML private MediaView mediaView;    
+    @FXML private Button playPauseButton;
+    private MediaPlayer mediaPlayer;    
+    private boolean playing = false;
     
+    public void initialize() {  //URL url, ResourceBundle rb) {
+        URL url = VideoPlayerController.class.getResource("test.mp4");        
+        Media media = new Media(url.toExternalForm());
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+        mediaPlayer.setOnEndOfMedia(
+            new Runnable() {
+                public void run() {
+                    playing = false;
+                    playPauseButton.setText("Play");
+                    mediaPlayer.seek(Duration.ZERO);
+                    mediaPlayer.pause();
+                }
+            }
+        );
+    
+        mediaPlayer.setOnError(
+            new Runnable() {
+                public void run() {
+/*                    ExceptionDialogs dialog =
+                        new ExceptionDialog(mediaPlayer.getError());
+                    dialog.showAndWait();
+ */               }
+            }
+        );
+        
+        mediaPlayer.setOnReady(
+            new Runnable() {
+                public void run() {
+                    DoubleProperty width = mediaView.fitWidthProperty();
+                    DoubleProperty height = mediaView.fitHeightProperty();
+                    width.bind(Bindings.selectDouble(
+                        mediaView.sceneProperty(), "width"));
+                    width.bind(Bindings.selectDouble(
+                        mediaView.sceneProperty(), "height"));
+                }
+            }
+        );
+    }  
+        
+        
+    @FXML 
+    private void playPauseButtonPressed(ActionEvent e) {
+        playing = !playing;
+        
+        if(playing) {
+            playPauseButton.setText("Pause");
+            mediaPlayer.play();
+        } else {
+            playPauseButton.setText("Play");
+            mediaPlayer.pause();
+
+        }
+    }
 }
