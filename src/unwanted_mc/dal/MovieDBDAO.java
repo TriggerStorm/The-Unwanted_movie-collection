253764e456b17bf255cb3cca81285d974c3d1e37
Martin Brodersen <martinroager@gmail.com>
2020-01-16 12:40:13 +0100

    Update MovieDBDAO.java

--- src/unwanted_mc/dal/MovieDBDAO.java
+++ src/unwanted_mc/dal/MovieDBDAO.java
@@ -26,31 +26,26 @@ import unwanted_mc.bll.DateConverter;
 /*
  * @author Niclas, Martin, Michael and Alan
  */
-
-
 public class MovieDBDAO {
-    
+
     DBConnection dbc = new DBConnection();
     private Movie movie; // = new Movie(1,"MovieTest1", 8,"src/Movie1.MP4", "11 January 2020" ); //TEST ONLY
     private DateConverter dateconverter = new DateConverter();  // Use manager later
-    
-    
-    
+
     public Movie getMovie(List<Movie> allMovies, int id) throws SQLException {
-         for (int i = 0; i < allMovies.size(); i++) {
+        for (int i = 0; i < allMovies.size(); i++) {
             Movie movie = allMovies.get(i);
             int movieId = movie.getId();
-            if (movieId == id)  {
-            return movie;
+            if (movieId == id) {
+                return movie;
             }
         }
         return null;
     }
-    
-    
-     public Movie addMovieToDB(String name, double rating, String filelink, String lastview) {
+
+    public Movie addMovieToDB(String name, double rating, String filelink, String lastview) {
         String sql = "INSERT INTO movie VALUES (?,?,?,?)";
-        try (Connection con = dbc.getConnection()) {
+        try ( Connection con = dbc.getConnection()) {
             PreparedStatement stmt = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
             stmt.setString(1, movie.getName());
             stmt.setDouble(2, movie.getRating());
@@ -62,7 +57,7 @@ public class MovieDBDAO {
                 throw new SQLException("Creating user failed, no rows affected.");
             }
 
-            try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+            try ( ResultSet generatedKeys = stmt.getGeneratedKeys()) {
                 if (generatedKeys.next()) {
                     movie.setId((int) generatedKeys.getLong(1));
                 } else {
@@ -76,11 +71,10 @@ public class MovieDBDAO {
         }
         return null;
     }
-     
-     
-      public void removeMovieFromDB(int id) {
+
+    public void removeMovieFromDB(int id) {
         String stat = "DELETE FROM movie WHERE ID=?";
-        try (Connection con = dbc.getConnection()) {
+        try ( Connection con = dbc.getConnection()) {
             PreparedStatement stmt = con.prepareStatement(stat);
             stmt.setInt(1, movie.getId());                      // IS THIS 0 ??
             stmt.execute();
@@ -89,8 +83,6 @@ public class MovieDBDAO {
         }
     }
 
-      
-      
     public List<Movie> fetchAllMovies() throws SQLException {
         List<Movie> allMovies = new ArrayList<>();
 
@@ -105,7 +97,7 @@ public class MovieDBDAO {
                 String filelink = rs.getString("filelink");
                 String lastview = rs.getString("lastview");
                 allMovies.add(new Movie(id, name, rating, filelink, lastview));
-                
+
             }
         } catch (SQLServerException ex) {
             Logger.getLogger(MovieDBDAO.class.getName()).log(Level.SEVERE, null, ex);
@@ -114,12 +106,42 @@ public class MovieDBDAO {
         }
         return allMovies;
     }
-      
-    
-    
+
+    public List<Movie> fetchIMBD() throws SQLException {
+  //      List<String> imbdList = new Arraylist();
+        List<Movie> movList = new ArrayList<>();
+        List<Movie> matchingMov = new ArrayList<>();
+        movList = fetchAllMovies();
+
+        try ( Connection con = dbc.getConnection()) {
+            String sql = "SELECT ratings.tconst, name.titleId, name.title, ratings.averageRating\n" + "FROM ratings INNER JOIN\n" + "name ON ratings.tconst = name.titleId";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            for (int i = 0; i < movList.size(); i++) {
+                Movie mov = getMovie(movList, i);
+                String movName = mov.getName();
+                while (rs.next()) {
+                    int id = rs.getInt("titleId");
+                    String name = rs.getString("title");
+                    int rating = rs.getInt("averageRating");
+
+                    if (movName == name) {
+                        matchingMov.add(mov);
+                    }
+                }
+            }
+            return matchingMov; 
+        } catch (SQLServerException ex) {
+            Logger.getLogger(MovieDBDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(MovieDBDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
     public Movie editMovie(String name, int rating, String filelink, String lastview) {
         try (//Get a connection to the database.
-            Connection con = dbc.getConnection()) {
+                 Connection con = dbc.getConnection()) {
             //Create a prepared statement.
             String sql = "UPDATE movie SET name = ?, rating = ?, filelink = ?, lastview = ? WHERE id = ?";
             PreparedStatement pstmt = con.prepareStatement(sql);
@@ -143,38 +165,35 @@ public class MovieDBDAO {
         return null;
     }
 
-    
-    
     public List<Movie> findMoviesToRemove() throws SQLException {  // Creates a list of movies that have a rating below 6, and haven't been played in two years.
         List<Movie> allMovies = new ArrayList<>();
         List<Movie> moviesToDelete = new ArrayList<>();
-    //    int expiry =  parseInt(movie.getLastView());
+        //    int expiry =  parseInt(movie.getLastView());
         allMovies = fetchAllMovies();
-        for(Movie movie : allMovies) {
+        for (Movie movie : allMovies) {
             String lastViewedDate = movie.getLastView();
             int movieId = movie.getId();
             boolean overTwoYears = testForLastView(movieId);
             if ((movie.getRating() < 6) && (overTwoYears)) {
-            moviesToDelete.add(movie);
+                moviesToDelete.add(movie);
             }
         }
         return moviesToDelete;
     }
-    
-    
+
     public void updateLastView(int id, String dateNow) {
         try (//Get a connection to the database.
-            Connection con = dbc.getConnection()) {
+                 Connection con = dbc.getConnection()) {
             //Create a prepared statement.
 
             String sql = "UPDATE movie SET lastview = dateNow WHERE id = ?";
             PreparedStatement pstmt = con.prepareStatement(sql);
             //Set parameter values.
-         
+
             pstmt.setString(4, dateNow);
             //Execute SQL query.
             pstmt.executeUpdate();
-           
+
             movie.setLastView(dateNow);
         } catch (SQLServerException ex) {
             Logger.getLogger(MovieDBDAO.class.getName()).log(Level.SEVERE, null, ex);
@@ -182,8 +201,7 @@ public class MovieDBDAO {
             Logger.getLogger(MovieDBDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    
+
     public boolean testForLastView(int id) throws SQLException {
         boolean overTwoYears = false;
         List<Movie> allMovies = fetchAllMovies();
@@ -201,5 +219,4 @@ public class MovieDBDAO {
         return overTwoYears;
     }
 
-    
 }
